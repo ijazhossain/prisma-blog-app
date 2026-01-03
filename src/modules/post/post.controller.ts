7455b2d4ad93dc37f0ded9d95eee0560abe45038
ijazhossain <ijaz.hossain35@gmail.com>
2026-01-03 12:57:53 +0600

    helper function added

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { postService } from "./post.service";
 import { PostStatus } from "../../../generated/prisma/enums";
+import paginationSortingHelper from "../../helpers/paginationSortingHelper";
 
 const createPost = async (req: Request, res: Response) => {
   try {
@@ -34,15 +35,18 @@ const getAllPost = async (req: Request, res: Response) => {
         : undefined
       : undefined;
     // console.log({ isFeatured });
-    const status=req.query.status as PostStatus | undefined;
-    const authorId=req.query.authorId as string | undefined;
-    const page=Number(req.query.page??1);
-    
-    const limit=Number(req.query.limit??10);
-const skip=(page-1)*limit;
-console.log({page,limit,skip});
-const sortBy=req.query.sortBy as string | undefined;
-const sortOrder=req.query.sortOrder as string | undefined;
+    const status = req.query.status as PostStatus | undefined;
+    const authorId = req.query.authorId as string | undefined;
+    //     const page=Number(req.query.page??1);
+
+    //     const limit=Number(req.query.limit??10);
+    // const skip=(page-1)*limit;
+    // console.log({page,limit,skip});
+    // const sortBy=req.query.sortBy as string | undefined;
+    // const sortOrder=req.query.sortOrder as string | undefined;
+    const { page, limit, skip, sortBy, sortOrder } = paginationSortingHelper(
+      req.query
+    );
 
     const result = await postService.getAllPostFromDB({
       search: searchString,
@@ -54,7 +58,7 @@ const sortOrder=req.query.sortOrder as string | undefined;
       limit,
       skip,
       sortBy,
-      sortOrder
+      sortOrder,
     });
     res.status(200).json(result);
   } catch (err: any) {
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -34,8 +34,8 @@ const getAllPostFromDB = async ({
   page: number;
   limit: number;
   skip: number;
-  sortBy: string| undefined;
-  sortOrder: string| undefined;
+  sortBy: string;
+  sortOrder: string;
 }) => {
   const andConditions: PostWhereInput[] = [];
   if (search) {
@@ -89,9 +89,9 @@ const getAllPostFromDB = async ({
     where: {
       AND: andConditions,
     },
-    orderBy:sortBy && sortOrder ?{
+    orderBy:{
       [sortBy]:sortOrder
-    }:{createdAt:"desc"}
+    }
   });
   return allPost;
 };
