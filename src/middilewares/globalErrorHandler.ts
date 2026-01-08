0205a56a2b7b8c9a4db22423b12b88ad38e06150
ijazhossain <ijaz.hossain35@gmail.com>
2026-01-08 21:35:36 +0600

    error handler added

--- src/middilewares/globalErrorHandler.ts
+++ src/middilewares/globalErrorHandler.ts
@@ -29,6 +29,21 @@ function errorHandler(
       errorMessage = "Foreign key constraint failed";
     }
   }
+  // PrismaClientUnknownRequestError
+  else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+    statusCode = 500;
+    errorMessage = "Error occurred during query execution";
+  }
+  //PrismaClientInitializationError
+  else if (err instanceof Prisma.PrismaClientInitializationError) {
+    if (err.errorCode === "P1000") {
+      statusCode = 401;
+      errorMessage = "Authentication failed. Please check your credentials!";
+    } else if (err.errorCode === "P1001") {
+      statusCode = 400;
+      errorMessage = "Cn not reach database server";
+    }
+  }
 
   res.status(statusCode);
   res.json({
